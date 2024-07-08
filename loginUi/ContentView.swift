120290196c8814468ae2bbfc818a23dfed77cac8
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-07-09 01:17:22 +0700

    Initial Commit

---
+++ loginUi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  loginUi
+//
+//  Created by Ricky Primayuda Putra on 09/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ loginUi/loginUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginUiApp.swift
+//  loginUi
+//
+//  Created by Ricky Primayuda Putra on 09/07/24.
+//
+
+import SwiftUI
+
+@main
+struct loginUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
